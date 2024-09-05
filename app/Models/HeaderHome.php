1fdc3ba0b42db8c146605816061a86ac2ddffeaa
ariyapratma <apanpratama1103@gmail.com>
2024-09-05 19:47:42 +0700

    first commit

---
+++ app/Models/HeaderHome.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HeaderHome extends Model
+{
+    use HasFactory;
+
+    protected $table = 'header_home';
+
+    protected $fillable = [
+        'title',
+        'description',
+        'buttonText',
+        'imageUrl',
+        'whatsappLink',
+    ];
+}
