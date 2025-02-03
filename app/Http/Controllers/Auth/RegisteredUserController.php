b878940f0e28d3f3ec9dabfa10c1509debfd03e6
Ariya <apanpratama1103@gmail.com>
2025-02-03 08:36:40 +0700

    Fix RegisteredUserController

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,51 +32,6 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    // public function store(Request $request): RedirectResponse
-    // {
-    //     $request->validate([
-    //         'name' => 'required|string|max:255',
-    //         'email' => 'required|string|lowercase|email|max:255|unique:' . User::class,
-    //         'password' => ['required', 'confirmed', Rules\Password::defaults()],
-    //     ]);
-
-    //     // Create roles 'user' and 'admin' if not already present
-    //     Role::firstOrCreate(['name' => 'user']);
-    //     Role::firstOrCreate(['name' => 'admin']);
-
-    //     // Create the user
-    //     $user = User::create([
-    //         'name' => $request->name,
-    //         'email' => $request->email,
-    //         'password' => Hash::make($request->password),
-    //     ]);
-
-    //     // Generate avatar and save it to the storage
-    //     $avatar = new Avatar();
-    //     $avatar->create($request->name)
-    //         ->save(storage_path('app/public/avatars/' . $user->id . '.png'));
-
-    //     // Assign default 'user' role
-    //     $user->assignRole('user');
-
-    //     // Assign 'admin' role for specific email address
-    //     // if ($request->email === 'adminbioqueen@indonesia.com') {
-    //     //     $user->assignRole('admin');
-    //     // }
-
-    //     if (strtolower($request->email) === 'adminbioqueen@indonesia.com') {
-    //         $user->assignRole('admin');
-    //     }
-
-
-    //     // Trigger the registered event
-    //     event(new Registered($user));
-
-    //     // Log in the user
-    //     Auth::login($user);
-
-    //     return redirect(RouteServiceProvider::HOME);
-    // }
 
     public function store(Request $request): RedirectResponse
     {
